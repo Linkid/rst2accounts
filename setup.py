b7149a2b257703f6b5a0bfe498ffb030c4dcab4c
François Magimel <francois.magimel@alumni.enseeiht.fr>
2016-08-28 09:31:30 +0200

    Fix typo in the setup file

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     name='rst2accounts',
     version='0.1',
     description='Complete your accounts in ReST',
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     author=u'François Magimel',
     url='https://github.com/Linkid/rst2accounts',
     license='BSD',
-    packages=find_packages()
+    packages=find_packages(),
     classifiers=[
         "Environment :: Console",
         "License :: OSI Approved :: BSD License",
